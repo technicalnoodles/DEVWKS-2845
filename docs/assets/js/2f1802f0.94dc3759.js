2f58a08527fde038d1cb0c0725c9a55b62cbd87b
galica9999 <galica9999@gmail.com>
2023-01-13 22:57:03 -0500

    docs

---
+++ docs/assets/js/2f1802f0.94dc3759.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklambda_docs=self.webpackChunklambda_docs||[]).push([[385],{3769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ docs/assets/js/f6a717f9.80347c96.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklambda_docs=self.webpackChunklambda_docs||[]).push([[595],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
